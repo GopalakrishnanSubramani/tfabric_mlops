8ae4f4f95437542b6398834806fc351a68dd0bad
Gopalakrishnan Subramani <92360012+GopalakrishnanSubramani@users.noreply.github.com>
2023-02-12 20:36:47 +0200

    adding web app config files

---
+++ app.py
@@ -0,0 +1,54 @@
+import joblib
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+import json
+
+app = Flask(__name__)
+
+#load the model
+model = joblib.load("./RF.bin")
+scaling = joblib.load("./scaling.bin")
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
+@app.route('/')
+def home():
+    return  render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaling.transform(np.array(list(data.values())).reshape(1,-1))
+    output = model.predict(new_data)
+    print(output[0])
+    return json.dumps(output[0],default=str)
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaling.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template("home.html", prediction_text= "The person is {}".format(output))
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
